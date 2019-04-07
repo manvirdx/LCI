8cd97d16b29793a2360d5040a16c40256733f517
Manvir <32556055+manvirdx@users.noreply.github.com>
2019-04-07 20:19:50 +0100

    Update LambdaCalculus.hs
    Added parts 2-5

--- LambdaCalculus.hs
+++ LambdaCalculus.hs
@@ -27,3 +27,32 @@ numeral i = Lambda "f" (Lambda "x" (numeralChurch (i)))
     where
         numeralChurch 0 = Variable "x"
         numeralChurch i = Apply (Variable "f") (numeralChurch(i-1))
+        
+-------------------------------------
+---Part 2: Variable list generator---
+-------------------------------------
+variables :: [Var]
+variables = [[a] | a <- ['a'..'z']] ++ (alphabetList 1)
+    where
+        alphabetList :: Int -> [Var]
+        alphabetList i = [[a] ++ (show i) | a <- ['a'..'z']] ++ (alphabetList (i+1))
+        
+-------------------------------------
+----Part 3: Variable lists filter----
+-------------------------------------
+filterVariables :: [Var] -> [Var] -> [Var]
+filterVariables listA listB = [x | x <- listA, not (elem x listB)]
+
+-------------------------------------
+-----Part 4: Variable generation-----
+-------------------------------------
+fresh :: [Var] -> Var
+fresh list = head (filterVariables variables list)
+
+-------------------------------------
+-------Part 5: Unused variable-------
+-------------------------------------
+used :: Term -> [Var]
+used (Variable v) = [v]
+used (Lambda x term) = merge [x] (used term)
+used (Apply term1 term2) = merge (used term1) (used term2)
