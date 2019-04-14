23f549588eff9f96506a21208a1b3c6603506e7d
Manvir <32556055+manvirdx@users.noreply.github.com>
2019-04-14 18:39:48 +0100

    Update LambdaCalculus.hs

--- LambdaCalculus.hs
+++ LambdaCalculus.hs
@@ -74,3 +74,48 @@ substitute x f (Apply  n m) = Apply (substitute x f n) (substitute x f m)
 substitute x f (Lambda z n) = if x == z then (Lambda z n) 
                               else (Lambda ld (substitute x f (rename z (ld) n))) 
                               where ld = fresh(merge [x] (merge (used f) (used n)))
+                              
+-------------------------------------
+--------Part 8: Beta reduction-------
+-------------------------------------
+beta :: Term -> [Term]
+beta (Apply (Lambda z n) m) = (substitute z m n):[Apply (Lambda z nn) m | nn <- (beta n)] ++ [Apply (Lambda z n) nm | nm <- (beta m)]
+beta (Apply n m) = [Apply nn m | nn <- (beta n)] ++ [Apply n nm | nm <- beta(m)]
+beta (Lambda n m) = [Lambda n beta_m | beta_m <- beta m]
+beta (Variable v) = []
+
+-------------------------------------
+----Part 9: Normal form reduction----
+-------------------------------------
+normalize :: Term -> IO ()
+normalize (toNormalize) = do 
+                                print(toNormalize) 
+                                let normalizing = (beta (toNormalize))
+                                if (length (normalizing) > 0) 
+                                then normalize (head (normalizing)) 
+                                else return ()
+                                
+-------------------------------------
+---Part 10: Applicative reduction----
+------------------------------------- 
+a_beta :: Term -> [Term]
+a_beta (Apply (Lambda z n) m) = [Apply (Lambda z nn) m | nn <- (a_beta n)] ++ [Apply (Lambda z n) nm | nm <- (a_beta m)] ++ [(substitute z m n)]
+a_beta (Apply n m) = [Apply nn m | nn <- (a_beta n)] ++ [Apply n nm | nm <- a_beta(m)]
+a_beta (Lambda n m) = [Lambda n a_beta
+
+-------------------------------------
+---Part 11: Applicative normalize----
+-------------------------------------
+a_normalize :: Term -> IO ()
+a_normalize (a_toNormalize) = do 
+                                print(a_toNormalize) 
+                                let a_normalizing = (a_beta (a_toNormalize))
+                                if (length (a_normalizing) > 0) 
+                                then a_normalize (head (a_normalizing)) 
+                                else return ()
+                                
+ ------------------------------------
+-----Part 12: Normalize test cases---
+-------------------------------------                               
+
+
